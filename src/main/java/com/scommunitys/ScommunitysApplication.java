85b428fb3da2dfb551a8ff445bb51bb5db4dece8
sss <2140327352@qq.com>
2023-05-07 14:44:16 +0800

    lll

---
+++ src/main/java/com/scommunitys/ScommunitysApplication.java
@@ -0,0 +1,13 @@
+package com.scommunitys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScommunitysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScommunitysApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/scommunitys/d/w.java
@@ -0,0 +1,17 @@
+package com.scommunitys.d;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author sss
+ * @version 1.0
+ * @date 2023/5/6 18:19
+ */
+@Controller
+public class w {
+    @RequestMapping("/s")
+    public String s(){
+        return "sdf";
+    }
+}
